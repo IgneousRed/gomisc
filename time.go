3b0df74dc495d54e810c2c3682fb7aa0b9817f97
IgneousRed <IgneousRed@protonmail.com>
2023-01-01 18:16:23 +0100

    time fix

--- time.go
+++ time.go
@@ -8,14 +8,14 @@ type Micros int64
 type Nanos int64
 
 func SecondsGet() Seconds {
-	return Seconds(time.Now().UnixMilli())
+	return Seconds(time.Now().Unix())
 }
 func MillisGet() Millis {
 	return Millis(time.Now().UnixMilli())
 }
 func MicrosGet() Micros {
-	return Micros(time.Now().UnixMilli())
+	return Micros(time.Now().UnixMicro())
 }
 func NanosGet() Nanos {
-	return Nanos(time.Now().UnixMilli())
+	return Nanos(time.Now().UnixNano())
 }
